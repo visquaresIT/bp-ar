f0f608151ec08e738a6c96b6ca36a9c30871e336
Achmat Fauzi <achmatfauzi86@gmail.com>
2026-05-18 00:13:04 +0700

    update model

--- src/includes/App.js
+++ src/includes/App.js
@@ -19,8 +19,8 @@ class App {
     this.markerConfigs = [
       { file: '/models/train.glb', clipIndex: 0 }, // 0: 60 YEARS logo
       { file: '/models/train.glb', clipIndex: 0 }, // 1: Indonesia map
-      { file: '/models/train.glb', clipIndex: 0 }, // 2: Square pattern
-      { file: '/models/bp-jacket-opt-1.glb', clipIndex: 0 }, // 3: Tag / container
+      { file: '/models/bp-jacket.glb', clipIndex: 0 }, // 2: Square pattern
+      { file: '/models/train.glb', clipIndex: 0 }, // 3: Tag / container
       { file: '/models/train.glb', clipIndex: 0 }, // 4: CO2
       { file: '/models/train.glb', clipIndex: 0 }, // 5: Bor
       { file: '/models/train.glb', clipIndex: 0 }, // 6: CO2 solid
